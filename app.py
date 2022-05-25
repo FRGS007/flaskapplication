af9c686b47f9536e316f944e9e75e21aa80960c5
Maciej Kosowski <maciej_kosowski@wp.pl>
2022-05-25 04:30:25 +0200

    add register

--- app.py
+++ app.py
@@ -4,9 +4,13 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
+def helloLog():
     log = False
     return render_template('hello.html', log=log)
+
+def helloReg():
+    reg = False
+    return render_template('hello.html', reg=reg)
 
 
 @app.route('/signin', methods=['POST', 'GET'])
@@ -14,9 +18,20 @@ def logging():
     if request.method == 'POST':
         login = request.form['login']
         password = request.form['password']
-        if login == 'maciekpl' and password == 'haslo123':
-            return "<p>Sign in successful!</p>"
-    return render_template('signin.html')
+        if login == 'login' and password == 'pass':
+            return "<p>Login successful!</p>"
+    return render_template('login.html')
+
+
+@app.route('/signup', methods=['POST', 'GET'])
+def register():
+    if request.method == 'POST':
+        firstName = request.form['firstName']
+        lastName = request.form['lastName']
+        email = request.form['email']
+        password = request.form['password']
+        return "<p>Registration successful</p>" #nie dziala
+    return render_template('register.html')
 
 
 if __name__ == '__main__':
