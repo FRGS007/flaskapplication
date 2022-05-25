a9c2b784eb0ee8faad8de9566788d38a856279af
Maciej Kosowski <maciej_kosowski@wp.pl>
2022-05-25 03:01:37 +0200

    add login

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def hello():
+    log = False
+    return render_template('hello.html', log=log)
+
+
+@app.route('/signin', methods=['POST', 'GET'])
+def logging():
+    if request.method == 'POST':
+        login = request.form['login']
+        password = request.form['password']
+        if login == 'maciekpl' and password == 'haslo123':
+            return "<p>Sign in successful!</p>"
+    return render_template('signin.html')
 
 
 if __name__ == '__main__':
